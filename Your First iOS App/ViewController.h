c27676a0a54e4430105a65a03cc5894a590c6a50
Paymahn Moghadasian <paymahn1@gmail.com>
2013-08-05 11:30:28 -0500

    Finished my first ios app

--- Your First iOS App/ViewController.h
+++ Your First iOS App/ViewController.h
@@ -8,6 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate>
+- (IBAction)changeGreeting:(id)sender;
 
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@property (copy, nonatomic) NSString *userName;
 @end
